0f7f876267afe19e3a26e8001feeb855933573ae
JN513 <jn5.guape@gmail.com>
2021-12-31 22:23:45 -0300

    display component

---
+++ main/esp_display.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+#include <string.h> // Inclui biblioteca de funções de manipulação de strings
+
+#include "esp_system.h" // Biblioteca de funções do sistema
+#include "esp_event.h" // Biblioteca de eventos
+#include "esp_log.h" // Biblioteca de log
+
+#include "freertos/FreeRTOS.h" // Biblioteca de sistema de tarefas
+#include "freertos/task.h" // Biblioteca de tarefas
+#include "freertos/semphr.h" // Biblioteca de sistema de mutex
+#include "freertos/queue.h" // Biblioteca de filas
+
+static const char * TAG = "ESP_DISPLAY"; // Tag de log
+
+void app_main(void)
+{
+    
+}
